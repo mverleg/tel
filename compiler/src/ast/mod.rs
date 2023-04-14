86ccca5a1541fbd67c25c7dbc0e91ae6325090ac
Mark <mverleg.noreply@gmail.com>
2023-04-14 22:46:28 +0200

    Add identifier type

--- compiler/src/ast/mod.rs
+++ compiler/src/ast/mod.rs
@@ -7,6 +7,34 @@ pub enum OpCode {
     Div,
 }
 
+#[derive(Debug, PartialEq, Eq)]
+pub struct Identifier {
+    name: String,
+}
+
+impl Identifier {
+    pub fn new(name: impl Into<String>) -> Option<Self> {
+        let name = name.into();
+        for ch in name.chars() {
+            match ch {
+                '0'..='9' => {},
+                'a'..='z' => {},
+                'Z'..='Z' => {},
+                '_' => {},
+                _ => return None,
+            }
+        }
+        let first = name.chars().next()?;
+        match first {
+            'a'..='z' => {},
+            'A'..='Z' => {},
+            //TODO @mark: allow _ as leading char?
+            _ => return None,
+        }
+        Some(Identifier { name })
+    }
+}
+
 #[derive(Debug)]
 pub enum Expr {
     Num(f64),
