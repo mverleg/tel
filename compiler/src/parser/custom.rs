c0ffee789777ed9e7dea7379288a8c7a7cb67769
Mark <mverleg.noreply@gmail.com>
2023-04-16 15:28:52 +0200

    Not seeing why () are parsed repeatedly

--- compiler/src/parser/custom.rs
+++ compiler/src/parser/custom.rs
@@ -41,32 +41,34 @@ static RE: LazyLock<TokenizerRegexes> = LazyLock::new(|| {
     re
 });
 
-pub fn tokenize(src_pth: PathBuf, code: &str) -> Result<Vec<Token>, SteelErr> {
+pub fn tokenize(src_pth: PathBuf, full_code: &str) -> Result<Vec<Token>, SteelErr> {
     let mut tokens = Vec::new();
     let mut ix = 0;
-    while ix < code.len() {
-        eprintln!("ix={ix} ch='{}'", code[ix..].chars().next().unwrap());  //TODO @mark: TEMPORARY! REMOVE THIS!
-        if let Some(caps) = RE.parenthesis_open_re.captures_iter(&code[ix..]).next() {
+    while ix < full_code.len() {
+        //TODO @mark: drop '...\n' continuations
+        let code = &full_code[ix..];
+        eprintln!("ix={ix} ch='{}'", code.chars().next().unwrap());  //TODO @mark: TEMPORARY! REMOVE THIS!
+        if let Some(caps) = RE.parenthesis_open_re.captures_iter(code).next() {
             let cap = caps.get(0).unwrap().as_str();
             let token = Token::ParenthesisOpen;
-            eprintln!("match {token:?} from {ix} to {}", ix + cap.len());
+            eprintln!("match {token:?} in '{cap}' from {ix} to {}", ix + cap.len());
             //TODO @mark: change to trace ^
             tokens.push(token);
             ix += cap.len();
             debug_assert!(cap.len() > 0);
             continue;
         }
-        if let Some(caps) = RE.parenthesis_close_re.captures_iter(&code[ix..]).next() {
+        if let Some(caps) = RE.parenthesis_close_re.captures_iter(code).next() {
             let cap = caps.get(0).unwrap().as_str();
             let token = Token::ParenthesisClose;
-            eprintln!("match {token:?} from {ix} to {}", ix + cap.len());
+            eprintln!("match {token:?} in '{cap}' from {ix} to {}", ix + cap.len());
             //TODO @mark: change to trace ^
             tokens.push(token);
             ix += cap.len();
             debug_assert!(cap.len() > 0);
             continue;
         }
-        if let Some(caps) = RE.op_symbol_re.captures_iter(&code[ix..]).next() {
+        if let Some(caps) = RE.op_symbol_re.captures_iter(code).next() {
             let cap = caps.get(0).unwrap().as_str();
             eprintln!("sym cap = '{cap}'");  //TODO @mark: TEMPORARY! REMOVE THIS!
             let sym = caps.get(1).unwrap().as_str();
@@ -77,7 +79,7 @@ pub fn tokenize(src_pth: PathBuf, code: &str) -> Result<Vec<Token>, SteelErr> {
                 "/" => OpCode::Div,
                 _ => unreachable!(),
             });
-            eprintln!("match {token:?} from {ix} to {}", ix + cap.len());
+            eprintln!("match {token:?} in '{cap}' from {ix} to {}", ix + cap.len());
             tokens.push(token);
             //TODO @mark: change to trace ^
             ix += cap.len();
@@ -96,6 +98,12 @@ mod tokens {
     #[test]
     fn allow_whitespace_after_open_parenthesis() {
         let tokens = tokenize(PathBuf::from("test"), "(\n)");
+        assert_eq!(tokens, Ok(vec![Token::ParenthesisOpen, Token::ParenthesisClose]));
+    }
+
+    #[test]
+    fn handle_non_ascii_strings() {
+        let tokens = tokenize(PathBuf::from("test"), "\"你好\"");
         assert_eq!(tokens, Ok(vec![Token::ParenthesisOpen, Token::ParenthesisClose]));
     }
 
