c0ffeedf15529a045bb63d4fbfcbbbfeb4a2fc89
Mark <mverleg.noreply@gmail.com>
2023-04-16 16:11:45 +0200

    Rewrite tokenizer to struct per operation

--- compiler/src/parser/custom.rs
+++ compiler/src/parser/custom.rs
@@ -17,7 +17,7 @@ pub fn parse_str(src_pth: PathBuf, code: &str) -> Result<AST, SteelErr> {
     unimplemented!()  //TODO @mark:
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Token {
     ParenthesisOpen,
     ParenthesisClose,
@@ -25,37 +25,67 @@ pub enum Token {
     Number(f64),
 }
 
-struct ParenthesisOpenTokenizer(Regex);
-
 trait Tokenizer {
     fn regex(&self) -> &Regex;
 
-    fn capture_handler(&self, cap_group: Option<&str>) -> Token;
+    fn token_for(&self, cap_group: Option<&str>) -> Token;
 }
 
-impl Tokenizer for ParenthesisOpenTokenizer {
+struct FixedTokenTokenizer(Regex, Token);
+
+impl Tokenizer for FixedTokenTokenizer {
     fn regex(&self) -> &Regex {
         &self.0
     }
 
-    fn capture_handler(&self, cap_group: Option<&str>) -> Token {
-        todo!()
+    fn token_for(&self, ignored: Option<&str>) -> Token {
+        debug_assert!(ignored.is_none(), "no capture group expected for this tokenizer");
+        self.1.clone()
+    }
+}
+
+struct OpSymbolTokenizer(Regex);
+
+impl Tokenizer for OpSymbolTokenizer {
+    fn regex(&self) -> &Regex {
+        &self.0
+    }
+
+    fn token_for(&self, op_sym: Option<&str>) -> Token {
+        Token::OpSymbol(match op_sym {
+            "+" => OpCode::Add,
+            "-" => OpCode::Sub,
+            "*" => OpCode::Mul,
+            "/" => OpCode::Div,
+            _ => unreachable!(),
+        })
+    }
+}
+
+struct NumberTokenizer(Regex);
+
+impl Tokenizer for NumberTokenizer {
+    fn regex(&self) -> &Regex {
+        &self.0
+    }
+
+    fn token_for(&self, num_repr: Option<&str>) -> Token {
+        match num_repr.parse() {
+            Ok(num) => Token::Number(num),
+            Err(_err) => unimplemented!(),  //TODO @mark: error handling (e.g. too large nr? most invalid input is handled by regex)
+        }
     }
 }
 
 //TODO @mark: pity about dyn, see if it gets optimized
-static RE: LazyLock<[dyn Tokenizer; 1]> = LazyLock::new(|| {
+static TOKENIZERS: LazyLock<[dyn Tokenizer; 4]> = LazyLock::new(|| {
     debug!("start creating tokenizers (compiling regexes)");
     let tokenizers = [
-        ParenthesisOpenTokenizer(Regex::new(r"^\s*\(\s*").unwrap()),
+        FixedTokenTokenizer(Regex::new(r"^\s*\(\s*").unwrap(), Token::ParenthesisOpen),
+        FixedTokenTokenizer(Regex::new(r"^\s*\)[ \t]*").unwrap(), Token::ParenthesisClose),
+        OpSymbolTokenizer(Regex::new(r"^\s*([*+\-/])\s*").unwrap()),
+        NumberTokenizer(Regex::new(r"^\s*\(\s*").unwrap()),
     ];
-    // let re = Tokenizers {
-    //     parenthesis_open_re: Regex::new(r"^\s*\(\s*").unwrap(),
-    //     parenthesis_close_re: Regex::new(r"^\s*\)[ \t]*").unwrap(),
-    //     op_symbol_re: Regex::new(r"^\s*([*+\-/])\s*").unwrap(),
-    //     number_re: Regex::new(r"^\s*(-?[0-9]+(?:\.[0-9]+)?)[ \t]*").unwrap(),
-    //     //TODO @mark: no exponential notation yet
-    // };
     debug!("finished creating tokenizers (compiling regexes)");
     tokenizers
 });
@@ -67,55 +97,18 @@ pub fn tokenize(src_pth: PathBuf, full_code: &str) -> Result<Vec<Token>, SteelEr
         //TODO @mark: drop '...\n' continuations
         let code = &full_code[ix..];
         eprintln!("ix={ix} code='{}'", code.chars().take(40).join(""));  //TODO @mark: TEMPORARY! REMOVE THIS!
-        if let Some(caps) = RE.parenthesis_open_re.captures_iter(code).next() {
-            let cap = caps.get(0).unwrap().as_str();
-            let token = Token::ParenthesisOpen;
-            eprintln!("match {token:?} in '{cap}' from {ix} to {}", ix + cap.len());
-            //TODO @mark: change to trace ^
-            tokens.push(token);
-            ix += cap.len();
-            debug_assert!(cap.len() > 0);
-            continue;
-        }
-        if let Some(caps) = RE.parenthesis_close_re.captures_iter(code).next() {
-            let cap = caps.get(0).unwrap().as_str();
-            let token = Token::ParenthesisClose;
-            eprintln!("match {token:?} in '{cap}' from {ix} to {}", ix + cap.len());
-            tokens.push(token);
-            ix += cap.len();
-            debug_assert!(cap.len() > 0);
-            continue;
-        }
-        if let Some(caps) = RE.op_symbol_re.captures_iter(code).next() {
-            let cap = caps.get(0).unwrap().as_str();
-            let sym = caps.get(1).unwrap().as_str();
-            eprintln!("sym cap = '{sym}'");  //TODO @mark: TEMPORARY! REMOVE THIS!
-            let token = Token::OpSymbol(match sym {
-                "+" => OpCode::Add,
-                "-" => OpCode::Sub,
-                "*" => OpCode::Mul,
-                "/" => OpCode::Div,
-                _ => unreachable!(),
-            });
-            eprintln!("match {token:?} in '{cap}' from {ix} to {}", ix + cap.len());
-            tokens.push(token);
-            ix += cap.len();
-            debug_assert!(cap.len() > 0);
-            continue;
-        }
-        if let Some(caps) = RE.number_re.captures_iter(code).next() {
-            let cap = caps.get(0).unwrap().as_str();
-            let num_repr = caps.get(1).unwrap().as_str();
-            eprintln!("num cap = '{num_repr}'");  //TODO @mark: TEMPORARY! REMOVE THIS!
-            let token = match num_repr.parse() {
-                Ok(num) => Token::Number(num),
-                Err(_err) => unimplemented!(),  //TODO @mark: error handling (e.g. too large nr? most invalid input is handled by regex)
-            };
-            eprintln!("match {token:?} in '{cap}' from {ix} to {}", ix + cap.len());
-            tokens.push(token);
-            ix += cap.len();
-            debug_assert!(cap.len() > 0);
-            continue;
+        for tokenizer in TOKENIZERS {
+            if let Some(caps) = tokenizer.regex().captures_iter(code).next() {
+                let mtch = caps.get(0).unwrap().as_str();
+                let grp = caps.get(0).map(|g| g.as_str());
+                let token = tokenizer.token_for(grp);
+                eprintln!("match {token:?} in '{cap}' from {ix} to {}", ix + grp.len());
+                //TODO @mark: change to trace ^
+                tokens.push(token);
+                ix += grp.len();
+                debug_assert!(grp.len() > 0);
+                continue;
+            }
         }
         unreachable!("unexpected end of input at #{ix} ('{}')", code.chars().next().unwrap())
     }
