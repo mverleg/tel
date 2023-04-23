c0ffee498a647b28200a0656ea0a3dbdc814bd4c
Mark <mverleg.noreply@gmail.com>
2023-04-23 22:38:03 +0200

    Throw away expression parsing code, impl only scalars for now

--- compiler/src/parser/custom.rs
+++ compiler/src/parser/custom.rs
@@ -97,96 +97,113 @@ fn parse_blocks(mut tokens: Cursor) -> Result<Vec<Block>, SteelErr> {
 }
 
 fn parse_expression(mut tokens: Cursor) -> ParseRes<Expr> {
-    match tokens.take() {
-        Some(Token::ParenthesisOpen) => {
-            let Ok((expr, mut tok)) = parse_expression(tokens) else {
-                debug!("tried to parse '('parenthesized')' group but did not find an expression after '('");
-                todo!("report error about missing )")  //TODO @mark:
-            };
-            if Some(&Token::ParenthesisClose) != tok.take() {
-                debug!("tried to parse '('parenthesized')' group but did not find closing at the end ')'");
-                todo!("report error about missing )")  //TODO @mark:
-            }
-            return Ok((expr, tok))
-        },
-        Some(Token::Identifier(iden)) => {
-            //TODO @mark: probably move this repetition down
-            let iden = Expr::Iden(iden.clone());
-            return if let Ok((expr, tok)) = parse_binary_op(tokens.fork(), iden) {
-                Ok((expr, tok))
-            } else {
-                todo!("report error - not sure how to get here, failed binary already falls back to left, which is available")  //TODO @mark:
-            };
-        },
-        Some(Token::Number(num)) => {
-            let num = Expr::Num(*num);
-            return if let Ok((expr, tok)) = parse_binary_op(tokens.fork(), num) {
-                Ok((expr, tok))
-            } else {
-                todo!("report error - not sure how to get here, failed binary already falls back to left, which is available")  //TODO @mark:
-            };
-        },
-        Some(Token::Text(txt)) => {
-            let txt = Expr::Text(txt.clone());
-            return if let Ok((expr, tok)) = parse_binary_op(tokens.fork(), txt) {
-                Ok((expr, tok))
-            } else {
-                todo!("report error - not sure how to get here, failed binary already falls back to left, which is available")  //TODO @mark:
-            };
-        },
-        Some(other) => todo!("error handling for unknown block start {other:?}"),
-        None => todo!("handle not finding an expression"),
-    }
+    parse_value(tokens)
+    // match tokens.take() {
+    //     Some(Token::ParenthesisOpen) => {
+    //         let Ok((expr, mut tok)) = parse_expression(tokens) else {
+    //             debug!("tried to parse '('parenthesized')' group but did not find an expression after '('");
+    //             todo!("report error about missing )")  //TODO @mark:
+    //         };
+    //         if Some(&Token::ParenthesisClose) != tok.take() {
+    //             debug!("tried to parse '('parenthesized')' group but did not find closing at the end ')'");
+    //             todo!("report error about missing )")  //TODO @mark:
+    //         }
+    //         return Ok((expr, tok))
+    //     },
+    //     Some(Token::Identifier(iden)) => {
+    //         //TODO @mark: probably move this repetition down
+    //         let iden = Expr::Iden(iden.clone());
+    //         return if let Ok((expr, tok)) = parse_binary_op(tokens.fork(), iden) {
+    //             Ok((expr, tok))
+    //         } else {
+    //             todo!("report error - not sure how to get here, failed binary already falls back to left, which is available")  //TODO @mark:
+    //         };
+    //     },
+    //     Some(Token::Number(num)) => {
+    //         let num = Expr::Num(*num);
+    //         return if let Ok((expr, tok)) = parse_binary_op(tokens.fork(), num) {
+    //             Ok((expr, tok))
+    //         } else {
+    //             todo!("report error - not sure how to get here, failed binary already falls back to left, which is available")  //TODO @mark:
+    //         };
+    //     },
+    //     Some(Token::Text(txt)) => {
+    //         let txt = Expr::Text(txt.clone());
+    //         return if let Ok((expr, tok)) = parse_binary_op(tokens.fork(), txt) {
+    //             Ok((expr, tok))
+    //         } else {
+    //             todo!("report error - not sure how to get here, failed binary already falls back to left, which is available")  //TODO @mark:
+    //         };
+    //     },
+    //     Some(other) => todo!("error handling for unknown block start {other:?}"),
+    //     None => todo!("handle not finding an expression"),
+    // }
     //TODO @mark: fail if there was no semicolon or newline (except ')' or '}' maybe? or maybe just forbid such onelines without ;)
 }
 
-//TODO @mark: this is addsub, rename?
-fn parse_binary_op(mut tokens: Cursor, left: Expr) -> ParseRes<Expr> {
-    let mut expr = left;
-    loop {
-        let op_code;
-        match tokens.peek() {
-            Some(Token::OpSymbol(op)) if op_code == OpCode::Add || op_code == OpCode::Sub => op_code = *op,
-            _ => return parse_bin_muldiv(tokens.fork(), expr),
-        }
-        tokens.take();
-        let (right, right_tokens) = parse_bin_muldiv(tokens.fork(), expr)?;
-        tokens = right_tokens;
-        expr = Expr::BinOp(op_code, Box::new(expr.clone()), Box::new(right));
-        //TODO @mark: is there a way to avoid cloning?
+fn parse_value(mut tokens: Cursor) -> ParseRes<Expr> {
+    match tokens.take() {
+        Some(Token::Identifier(iden)) => {
+            Ok((Expr::Iden(iden.clone()), tokens))
+        },
+        Some(Token::Number(num)) => {
+            Ok((Expr::Num(*num), tokens))
+        },
+        Some(Token::Text(txt)) => {
+            Ok((Expr::Text(txt.clone()), tokens))
+        },
+        Some(tok) => todo!("unknown token: {tok:?}"),
+        None => todo!("unexpected end of input"),
     }
 }
 
-fn parse_bin_muldiv(mut tokens: Cursor, left: Expr) -> ParseRes<Expr> {
-    //TODO @mark: TEMPORARY! REMOVE THIS!
-    let mut expr = left;
-    loop {
-        let op_code;
-        match tokens.peek() {
-            Some(Token::OpSymbol(op)) if op_code == OpCode::Mul || op_code == OpCode::Div => op_code = *op,
-            _ => return parse_bin_muldiv(tokens.fork(), &expr),
-        }
-        tokens.take();
-        let (right, right_tokens) = parse_bin_muldiv(tokens.fork(), &expr)?;
-        tokens = right_tokens;
-        expr = Expr::BinOp(op_code, Box::new(expr.clone()), Box::new(right));
-        //TODO @mark: is there a way to avoid cloning?
-    }
-}
-
-fn parse_binary_op_with(mut tokens: Cursor, left: Expr, op_condition: fn(&OpCode) -> bool, right_parser: impl FnMut(Cursor, Expr) -> ParseRes<Expr>) -> ParseRes<Expr> {
-    //TODO @mark: TEMPORARY! REMOVE THIS!
-    let mut expr = left;
-    loop {
-        let op_code;
-        match tokens.peek() {
-            Some(Token::OpSymbol(op)) if op_condition(op_code) => op_code = *op,
-            _ => return parse_bin_muldiv(tokens.fork(), &expr),
-        }
-        tokens.take();
-        let (right, right_tokens) = parse_bin_muldiv(tokens.fork(), &expr)?;
-        tokens = right_tokens;
-        expr = Expr::BinOp(op_code, Box::new(expr.clone()), Box::new(right));
-        //TODO @mark: is there a way to avoid cloning?
-    }
-}
+// //TODO @mark: this is addsub, rename?
+// fn parse_binary_op(mut tokens: Cursor, left: Expr) -> ParseRes<Expr> {
+//     let mut expr = left;
+//     loop {
+//         let op_code;
+//         match tokens.peek() {
+//             Some(Token::OpSymbol(op)) if op_code == OpCode::Add || op_code == OpCode::Sub => op_code = *op,
+//             _ => return parse_bin_muldiv(tokens.fork(), expr),
+//         }
+//         tokens.take();
+//         let (right, right_tokens) = parse_bin_muldiv(tokens.fork(), expr)?;
+//         tokens = right_tokens;
+//         expr = Expr::BinOp(op_code, Box::new(expr.clone()), Box::new(right));
+//         //TODO @mark: is there a way to avoid cloning?
+//     }
+// }
+//
+// fn parse_bin_muldiv(mut tokens: Cursor, left: Expr) -> ParseRes<Expr> {
+//     //TODO @mark: TEMPORARY! REMOVE THIS!
+//     let mut expr = left;
+//     loop {
+//         let op_code;
+//         match tokens.peek() {
+//             Some(Token::OpSymbol(op)) if op_code == OpCode::Mul || op_code == OpCode::Div => op_code = *op,
+//             _ => return parse_bin_muldiv(tokens.fork(), &expr),
+//         }
+//         tokens.take();
+//         let (right, right_tokens) = parse_bin_muldiv(tokens.fork(), &expr)?;
+//         tokens = right_tokens;
+//         expr = Expr::BinOp(op_code, Box::new(expr.clone()), Box::new(right));
+//         //TODO @mark: is there a way to avoid cloning?
+//     }
+// }
+//
+// fn parse_binary_op_with(mut tokens: Cursor, left: Expr, op_condition: fn(&OpCode) -> bool, right_parser: impl FnMut(Cursor, Expr) -> ParseRes<Expr>) -> ParseRes<Expr> {
+//     //TODO @mark: TEMPORARY! REMOVE THIS!
+//     let mut expr = left;
+//     loop {
+//         let op_code;
+//         match tokens.peek() {
+//             Some(Token::OpSymbol(op)) if op_condition(op_code) => op_code = *op,
+//             _ => return parse_bin_muldiv(tokens.fork(), &expr),
+//         }
+//         tokens.take();
+//         let (right, right_tokens) = parse_bin_muldiv(tokens.fork(), &expr)?;
+//         tokens = right_tokens;
+//         expr = Expr::BinOp(op_code, Box::new(expr.clone()), Box::new(right));
+//         //TODO @mark: is there a way to avoid cloning?
+//     }
+// }
