1b7a2c3b427c07965d1d9d0920590d08b7580ba9
Mark <mverleg.noreply@gmail.com>
2023-07-19 22:40:19 +0200

    Remove custom parser

--- compiler/src/parser/mod.rs
+++ compiler/src/parser/mod.rs
@@ -10,29 +10,27 @@ use crate::ast::Ast;
 use crate::parser::errors::build_error;
 use crate::SteelErr;
 
-mod custom;
 mod errors;
 mod lexer;
 
 // lalrpop_mod!(gen_parser, "/grammar.rs");
 
 pub fn parse_str(src_pth: PathBuf, code: &str) -> Result<Ast, SteelErr> {
-    self::custom::parse_str(src_pth, code)
-    // let parser = gen_parser::ProgParser::new();
-    // let res = parser.parse(code);
-    // match res {
-    //     Ok(ast) => {
-    //         debug!("ast: {:?}", &ast);
-    //         Ok(ast)
-    //     },
-    //     Err(err) => {
-    //         let (msg, line) = build_error(err, src_pth.to_str().unwrap(), code);
-    //         Err(SteelErr::ParseErr {
-    //             file: src_pth,
-    //             line,
-    //             msg,
-    //         })
-    //     }
-    // }
+    let parser = gen_parser::ProgParser::new();
+    let res = parser.parse(code);
+    match res {
+        Ok(ast) => {
+            debug!("ast: {:?}", &ast);
+            Ok(ast)
+        },
+        Err(err) => {
+            let (msg, line) = build_error(err, src_pth.to_str().unwrap(), code);
+            Err(SteelErr::ParseErr {
+                file: src_pth,
+                line,
+                msg,
+            })
+        }
+    }
     //TODO @mark: no unwrap
 }
