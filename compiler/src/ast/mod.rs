c0ffee77754101b8864dda3ebf741a6e612a3702
Mark <mverleg.noreply@gmail.com>
2023-08-20 16:44:14 +0200

    Simplify parsing code by adding a list+item function

--- compiler/src/ast/mod.rs
+++ compiler/src/ast/mod.rs
@@ -189,3 +189,10 @@ pub enum EnumVariant {
     Enum(Enum),
     Existing(Type),
 }
+
+pub fn vec_and<T>(mut items: Vec<T>, addition: Option<T>) -> Vec<T> {
+    if let Some(addition) = addition {
+        items.push(addition);
+    }
+    items
+}
