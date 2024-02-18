c0ffeeccfadc1761664042830abf2350c2d9e5dc
Mark <mverleg.noreply@gmail.com>
2024-02-18 14:59:29 +0100

    Solve CI errors

--- ast2code/src/lib.rs
+++ ast2code/src/lib.rs
@@ -2,5 +2,5 @@ use ::tel_api::TelFile;
 
 pub fn ast_to_code(_file: TelFile) -> String {
     eprintln!("ast_to_code not yet implemented");  //TODO @mark:
-    return "\n".to_owned()
+    "\n".to_owned()
 }
