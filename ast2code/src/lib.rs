c0ffee95a035d0b0037d7b3545ebc7a06c99cf07
Mark <mverleg.noreply@gmail.com>
2024-02-18 11:42:18 +0100

    Create a small module to convert ast to code

---
+++ ast2code/src/lib.rs
@@ -0,0 +1,5 @@
+use ::tel_api::telir::TelFile;
+
+pub fn ast_to_code(file: TelFile) -> String {
+    unimplemented!()
+}
--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,12 +1,18 @@
+use ::std::path::PathBuf;
+
 use ::rand::Rng;
 use ::rand::rngs::StdRng;
 use ::rand::SeedableRng;
 
-use tel_api::telir::TelFile;
+use ::tel::tel_build_str;
+use ::tel_api::telir::TelFile;
+use ::tel_ast_to_code::ast_to_code;
 
 fn main() {
     let rng: StdRng = SeedableRng::seed_from_u64(123_456_789);
     let file = gen_random_file(&rng);
+    let code = ast_to_code(file);
+    tel_build_str(PathBuf::from("script-input"), code, false).unwrap();
     unimplemented!();
 }
 
