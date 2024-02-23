c0ffee89f01a58a0bb455e52ee08b6ae0bf46cf7
Mark <mverleg.noreply@gmail.com>
2024-02-23 22:04:01 +0100

    Split functions/closures into module

---
+++ compiler/src/ast/function.rs
@@ -0,0 +1,21 @@
+use ::serde::Serialize;
+
+use crate::ast::Expr;
+use crate::ast::Block;
+use crate::ast::AssignmentDest;
+use crate::ast::Identifier;
+
+/// Can be a variable read or a function call. A function call without () cannot be differentiated from
+/// a function call by the parser, this must be done later.
+#[derive(Debug, PartialEq, Serialize)]
+pub struct Invoke {
+    pub iden: Identifier,
+    //TODO @mark: to smallvec or something:
+    pub args: Box<[Expr]>,
+}
+
+#[derive(Debug, PartialEq, Serialize)]
+pub struct Closure {
+    pub blocks: Box<[Block]>,
+    pub params: Box<[AssignmentDest]>,
+}
--- compiler/src/ast/mod.rs
+++ compiler/src/ast/mod.rs
@@ -11,6 +11,8 @@ pub use crate::ast::common::Type;
 pub use crate::ast::datatypes::Enum;
 pub use crate::ast::datatypes::EnumVariant;
 pub use crate::ast::datatypes::Struct;
+pub use crate::ast::function::Closure;
+pub use crate::ast::function::Invoke;
 
 pub use self::identifier::Identifier;
 
@@ -18,6 +20,7 @@ mod identifier;
 mod common;
 mod assign;
 mod datatypes;
+mod function;
 
 #[derive(Debug, PartialEq, Serialize)]
 pub struct Ast {
@@ -49,21 +52,6 @@ pub enum Expr {
     If(Box<[(Expr, Box<[Block]>)]>, Option<Box<[Block]>>),
     While(Box<Expr>, Box<[Block]>),
     ForEach(AssignmentDest, Box<Expr>, Box<[Block]>),
-}
-
-/// Can be a variable read or a function call. A function call without () cannot be differentiated from
-/// a function call by the parser, this must be done later.
-#[derive(Debug, PartialEq, Serialize)]
-pub struct Invoke {
-    pub iden: Identifier,
-    //TODO @mark: to smallvec or something:
-    pub args: Box<[Expr]>,
-}
-
-#[derive(Debug, PartialEq, Serialize)]
-pub struct Closure {
-    pub blocks: Box<[Block]>,
-    pub params: Box<[AssignmentDest]>,
 }
 
 pub fn vec_and<T>(mut items: Vec<T>, addition: Option<T>) -> Vec<T> {
