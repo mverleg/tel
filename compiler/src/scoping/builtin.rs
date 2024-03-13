c0ffee269e16205d208ebae0749823b441ade144
Mark <mverleg.noreply@gmail.com>
2024-03-13 22:46:40 +0100

    Attempt to do loop

--- compiler/src/scoping/builtin.rs
+++ compiler/src/scoping/builtin.rs
@@ -1,28 +1,29 @@
 
 //TODO @mark: delete?
 
-pub mod builtins {
-    pub const NEG: &'static str = "Negate.neg";
-    pub const MINUS: &'static str = "Minus.minus";
-}
+// pub mod builtins {
+//     pub const NEG: &'static str = "Negate.neg";
+//     pub const MINUS: &'static str = "Minus.minus";
+// }
 
 
 macro_rules! make_builtin_constants {
     ($iden: ident, $ex: expr) => {
         pub const $iden: &'static str = $ex;
     };
-    (($iden: ident, $ex: expr), $(($idens: ident, $exs: expr)),+) => {
+    (($iden: ident, $ex: expr), $($others: (ident, expr)),+) => {
         make_builtin_constants!(($iden, $ex));
     };
 }
 
 macro_rules! make_builtins {
     ($(($idens: ident, $exs: expr)),*) => {
-
+        make_builtin_constants!()
     };
 }
 
 make_builtins!(
     (NEG, "Negate.neg"),
-    (MINUS, "Minus.minus")
+    (MINUS, "Minus.minus"),
+    (TEST, "Test.test")
 );
