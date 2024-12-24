f59cc32b674ff04f54fdf89f1a215f23ec1a4834
Mark <mverleg.noreply@gmail.com>
2024-12-24 22:19:30 +0100

    Some types for the query design

--- qcompiler/src/lib.rs
+++ qcompiler/src/lib.rs
@@ -1,1 +1,39 @@
 
+trait FileSystem {
+    fn read(iden: FileIden) -> String;
+}
+
+#[derive(Debug)]
+struct FileIden {
+    iden: String,
+    //TODO @mark: should be lightweight, maybe convert to nr?
+}
+
+#[derive(Debug)]
+struct Import {
+    file: FileIden,
+    name: String,
+}
+
+#[derive(Debug)]
+struct FileCode {
+    text: String,
+}
+
+#[derive(Debug)]
+struct AST {
+}
+
+#[derive(Debug)]
+enum Query<F: FileSystem> {
+    Source(FileIden),
+    Parsed(FileIden),
+    Import(Import),
+}
+
+#[derive(Debug)]
+enum Answer {
+    Source(FileCode),
+    Parsed(AST),
+}
+
