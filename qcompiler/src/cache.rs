f67c6513d386282ca4273bfb4f84f74211a5049a
Mark <mverleg.noreply@gmail.com>
2024-12-25 23:49:22 +0100

    Part of the cache api, possibly

--- qcompiler/src/cache.rs
+++ qcompiler/src/cache.rs
@@ -13,6 +13,7 @@ use ::std::hash::Hash;
 /// * Good and fast hash codes and equals for keys, equals for values
 /// * Keys and values serde de/serializable (for disk)
 /// * Simultaneous immutable borrows of different keys can exist
+//TODO @mark: ^ let's start with memory only for the first version
 
 //TODO @mark: Mocka exists, but only memory
 
@@ -26,5 +27,39 @@ where
     K: Debug + PartialEq + Eq + Hash,
     V: Debug + PartialEq + Eq,
 {
+    //TODO @mark: should Option be default? or is peek rare and usually we use claim?
+    fn peek(&self, key: &K) -> Option<Res<&V>> {
+        todo!()
+    }
 
+    fn claim(&mut self, key: K) -> Either<Vacancy<V>, &V> {
+        todo!()
+    }
+}
+
+struct Res<V>
+where
+    V: Debug + PartialEq + Eq, {}
+
+impl<V> Res<V>
+where
+    V: Debug + PartialEq + Eq,
+{
+    //TODO @mark: async method, 'block' until ready
+    fn get(&self) -> &V {
+        todo!()
+    }
+}
+
+struct Vacancy<V>
+where
+    V: Debug + PartialEq + Eq, {}
+
+impl<V> Vacancy<V>
+where
+    V: Debug + PartialEq + Eq,
+{
+    fn insert(self, value: V) -> V {
+        todo!()
+    }
 }
