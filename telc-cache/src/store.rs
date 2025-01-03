d14f11c80e9535c01b5e9fa39df8e541ac39c686
Mark <mverleg.noreply@gmail.com>
2025-01-03 22:51:23 +0100

    Move cache to separate crate

--- telc-cache/src/store.rs
+++ telc-cache/src/store.rs
@@ -7,6 +7,7 @@ use ::std::hash::Hash;
 /// * Items should be the same after de/ser with serde, otherwise disk and memory won't behave the same.
 /// * Items are given unique u64 numbers. If this overflows, the cache is wiped.
 ///   Users should clear all their references, otherwise there may be collisions.
+/// * Not optimized for `Copy` types; assumes relatively big data.
 ///
 /// This is just the storage backend; hashing and lookups should happen in `db`.
 
@@ -14,7 +15,7 @@ struct Ref {
     ix: usize,
 }
 
-struct DiskStore<E> {
+struct DiskStore<E: serde::Serialize + serde::Deserialize> {
 
 }
 
@@ -22,7 +23,7 @@ struct MemoryStore<E> {
 
 }
 
-pub struct Store<E> {
+pub struct Store<E: serde::Serialize + serde::Deserialize> {
     top: Ref,
     disk: DiskStore<E>,
     memory: MemoryStore<E>,
